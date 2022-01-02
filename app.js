0d97b1d859d3e851b19b73511e410bbfee4e1972
Joe <Joe@DESKTOP-M95EUHJ>
2022-01-02 14:50:07 +0100

    feat: setup public directory to be served

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ const files = fs.readdirSync(dirPath).map(name => {
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -35,8 +34,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(express.static("public"));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
